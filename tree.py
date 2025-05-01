bac100510a1497954fe01d86a5696cb98ac094f7
Pavithran26 <137516256+Pavithran26@users.noreply.github.com>
2025-05-01 12:43:33 +0530

    Update tree.py

--- tree.py
+++ tree.py
@@ -2,8 +2,6 @@ height=3;
 width=4;
 space=height*width;
 m=1;
-
-#tree upper part
 for i in range(1,height+1):
     for x in range(m,width+1):
         for y in range(space,x+1,-1):
@@ -13,8 +11,6 @@ for i in range(1,height+1):
         print()
     m=m+2
     width=width+2
-
-#tree lower part
 for x in range(1,5):
     for y in range(space-5,0,-1):
         print(" ",end="")
